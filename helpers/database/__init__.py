d88fb5f8c6ddd6ee6e0d1d221c3c2fe00f428475
luan005 <bezerra.luan@academico.ifpb.edu.br>
2025-05-21 07:56:56 -0300

    Add files via upload

--- helpers/database/__init__.py
+++ helpers/database/__init__.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import g
+from helpers.application import app
 
 DATABASE = 'censoescolar.db'
 
@@ -8,3 +9,9 @@ def getConnection():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
+@app.teardown_appcontext
+def closeConnection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
--- helpers/logging/__init__.py
+++ helpers/logging/__init__.py
@@ -1,9 +1,15 @@
 import logging
-
-# Configuração básica do logger
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(message)s'
-)
+from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+fileHandler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
